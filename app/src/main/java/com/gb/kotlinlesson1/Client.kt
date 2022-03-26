3936a853cd7e334644c115c9e043ba70b1fb1a08
graymonk89 <graymonk89@yandex.ru>
2022-03-26 20:11:11 +0300

    Урок 1. Подготовка

---
+++ app/src/main/java/com/gb/kotlinlesson1/Client.kt
@@ -0,0 +1,5 @@
+package com.gb.kotlinlesson1
+
+data class Client(val id: Int,val name: String){
+
+}
---
+++ app/src/main/java/com/gb/kotlinlesson1/Order.kt
@@ -0,0 +1,3 @@
+package com.gb.kotlinlesson1
+
+data class Order(val number: Int, val paid: Boolean, val goodsId: Int)
